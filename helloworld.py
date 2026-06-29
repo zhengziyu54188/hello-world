6599034171d4011be81dd94a26c2bb9a80a4599d
yutou <zhengziyu54188@gmail.com>
2026-06-29 16:09:15 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,7 +4,4 @@ sum = 0
 for i in range(a):
     sum+=i**2
 print(sum)
-print('we are done,yeah!!!')
-print('your mother died')
-print('miku ni qian wo shiwang meidao')
-print('die die die')
+print('done')
